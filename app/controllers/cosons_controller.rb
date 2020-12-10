b8db5f98c4a67d55a2956eaa5b81cdafec299921
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-12-10 13:51:34 +0900

    cosonコントローラー作成

--- app/controllers/cosons_controller.rb
+++ app/controllers/cosons_controller.rb
@@ -1,4 +1,24 @@
 class CosonsController < ApplicationController
   def index
   end
+
+  def new
+    @cosons = Coson.new
+  end
+
+  def create
+    @coson = Coson.create(coson_params)
+    if @coson.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def coson_params
+    params.require(:coson).permit(:title, :info, :url, :image).merge(user_id: current_user.id)
+  end
 end
